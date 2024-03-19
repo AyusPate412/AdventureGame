d5208f5fff4b3dde91a69b85435adab524994772
AyusPate412 <payush44477@gmail.com>
2024-03-19 14:53:00 -0400

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleImage = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.titleImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleImage
+            // 
+            this.titleImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("titleImage.BackgroundImage")));
+            this.titleImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.titleImage.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.titleImage.Location = new System.Drawing.Point(12, 51);
+            this.titleImage.Name = "titleImage";
+            this.titleImage.Size = new System.Drawing.Size(284, 387);
+            this.titleImage.TabIndex = 0;
+            this.titleImage.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(156, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(458, 33);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // playButton
+            // 
+            this.playButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("playButton.BackgroundImage")));
+            this.playButton.Location = new System.Drawing.Point(490, 226);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(124, 51);
+            this.playButton.TabIndex = 2;
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(512, 114);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "label1";
+            // 
+            // option1Label
+            // 
+            this.option1Label.AutoSize = true;
+            this.option1Label.Location = new System.Drawing.Point(512, 156);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(35, 13);
+            this.option1Label.TabIndex = 4;
+            this.option1Label.Text = "label1";
+            this.option1Label.Click += new System.EventHandler(this.option1Label_Click);
+            // 
+            // option2Label
+            // 
+            this.option2Label.AutoSize = true;
+            this.option2Label.Location = new System.Drawing.Point(512, 180);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(35, 13);
+            this.option2Label.TabIndex = 5;
+            this.option2Label.Text = "label1";
+            this.option2Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.titleImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.titleImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox titleImage;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        //Track what page you are on
+        int page;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            int page = 1;
+        }
+        private void displayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You are lost in a forest";
+                    option1Label.Text = "Go North";
+                    option2Label.Text = "Go South";
+                    break;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void option1Label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
