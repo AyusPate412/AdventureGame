258c8fd902c8295ca3e98f52cd22c6a4e9b3a7ca
AyusPate412 <159576227+AyusPate412@users.noreply.github.com>
2024-03-21 11:27:39 -0400

    Update Form1.cs
    Added option 2 button

--- AdventureGame/Form1.cs
+++ AdventureGame/Form1.cs
@@ -34,6 +34,101 @@ namespace AdventureGame
                     break;
             }
         }
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;    
+            }
+            
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 3)
+            {
+                page = 25;
+            }
+            // Have to code the process here
+            else if (page == 4)
+            {
+                page = 1;
+            }
+            else if (page == 5)
+            {
+                page = 10;
+            }
+            else if (page == 6)
+            {
+                page = 5;
+            }
+            else if (page == 7)
+            {
+                page = 5;
+            }
+            else if (page == 8)
+            {
+                page = 5;
+            }
+            else if (page == 9)
+            {
+                page = 5;
+            }
+            else if (page == 10)
+            {
+                page = 5;
+            }
+            else if (page == 11)
+            {
+                page = 5;
+            }
+            else if (page == 12)
+            {
+                page = 5;
+            }
+            else if (page == 13)
+            {
+                page = 5;
+            }
+            else if (page == 14)
+            {
+                page = 5;
+            }
+            else if (page == 15)
+            {
+                page = 5;
+            }
+            else if (page == 16)
+            {
+                page = 5;
+            }
+            else if (page == 17)
+            {
+                page = 5;
+            }
+            else if (page == 18)
+            {
+                page = 5;
+            }
+            else if (page == 19)
+            {
+                page = 5;
+            }
+            else if (page == 20)
+            {
+                page = 5;
+            }
+            else if (page == 21)
+            {
+                page = 5;
+            }
+            
+            
+            
+            
+
+            DisplayPage();
+        }
 
         private void label1_Click(object sender, EventArgs e)
         {
