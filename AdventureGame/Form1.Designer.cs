52a327e4163b58283073a6da98fc7b2078ba1ca2
BuildTools <unconfigured@null.spigotmc.org>
2024-03-24 23:58:09 -0400

    Work from home

--- AdventureGame/Form1.Designer.cs
+++ AdventureGame/Form1.Designer.cs
@@ -30,13 +30,17 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.titleImage = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.titleLabel = new System.Windows.Forms.PictureBox();
             this.playButton = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
             this.option1Label = new System.Windows.Forms.Label();
             this.option2Label = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.titleImage)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // titleImage
@@ -44,89 +48,134 @@
             this.titleImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("titleImage.BackgroundImage")));
             this.titleImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.titleImage.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.titleImage.Location = new System.Drawing.Point(12, 51);
+            this.titleImage.Location = new System.Drawing.Point(16, 112);
+            this.titleImage.Margin = new System.Windows.Forms.Padding(4);
             this.titleImage.Name = "titleImage";
-            this.titleImage.Size = new System.Drawing.Size(284, 387);
+            this.titleImage.Size = new System.Drawing.Size(615, 664);
             this.titleImage.TabIndex = 0;
             this.titleImage.TabStop = false;
             // 
-            // pictureBox2
+            // titleLabel
             // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(156, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(458, 33);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.titleLabel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("titleLabel.BackgroundImage")));
+            this.titleLabel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.titleLabel.Location = new System.Drawing.Point(155, 26);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(802, 69);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.TabStop = false;
             // 
             // playButton
             // 
             this.playButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("playButton.BackgroundImage")));
-            this.playButton.Location = new System.Drawing.Point(490, 226);
+            this.playButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.playButton.Location = new System.Drawing.Point(798, 334);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(124, 51);
+            this.playButton.Size = new System.Drawing.Size(124, 55);
             this.playButton.TabIndex = 2;
             this.playButton.UseVisualStyleBackColor = true;
             this.playButton.Click += new System.EventHandler(this.playButton_Click);
             // 
             // outputLabel
             // 
-            this.outputLabel.AutoSize = true;
-            this.outputLabel.Location = new System.Drawing.Point(512, 114);
+            this.outputLabel.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(101, 15);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.Size = new System.Drawing.Size(840, 94);
             this.outputLabel.TabIndex = 3;
-            this.outputLabel.Text = "label1";
             // 
             // option1Label
             // 
-            this.option1Label.AutoSize = true;
-            this.option1Label.Location = new System.Drawing.Point(512, 156);
+            this.option1Label.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(621, 663);
+            this.option1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.option1Label.Name = "option1Label";
-            this.option1Label.Size = new System.Drawing.Size(35, 13);
+            this.option1Label.Size = new System.Drawing.Size(383, 36);
             this.option1Label.TabIndex = 4;
             this.option1Label.Text = "label1";
-            this.option1Label.Click += new System.EventHandler(this.option1Label_Click);
             // 
             // option2Label
             // 
-            this.option2Label.AutoSize = true;
-            this.option2Label.Location = new System.Drawing.Point(512, 180);
+            this.option2Label.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(621, 607);
+            this.option2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.option2Label.Name = "option2Label";
-            this.option2Label.Size = new System.Drawing.Size(35, 13);
+            this.option2Label.Size = new System.Drawing.Size(383, 38);
             this.option2Label.TabIndex = 5;
             this.option2Label.Text = "label1";
-            this.option2Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(155, 660);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(87, 38);
+            this.option1Button.TabIndex = 6;
+            this.option1Button.Text = "button1";
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(155, 605);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(87, 36);
+            this.option2Button.TabIndex = 7;
+            this.option2Button.Text = "button1";
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Image = global::AdventureGame.Properties.Resources.Designer;
+            this.mainPictureBox.Location = new System.Drawing.Point(16, 112);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(627, 468);
+            this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mainPictureBox.TabIndex = 9;
+            this.mainPictureBox.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1019, 821);
+            this.Controls.Add(this.mainPictureBox);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
             this.Controls.Add(this.option2Label);
             this.Controls.Add(this.option1Label);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.playButton);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.titleImage);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.titleImage)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox titleImage;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox titleLabel;
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Label outputLabel;
         private System.Windows.Forms.Label option1Label;
         private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.PictureBox mainPictureBox;
     }
 }
 
--- AdventureGame/Form1.cs
+++ AdventureGame/Form1.cs
@@ -7,43 +7,321 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Media;
 
 namespace AdventureGame
 {
     public partial class Form1 : Form
     {
         //Track what page you are on
-        int page;
+        int page = 1;
+
+        //new SoundPlayer introSound = new SoundPlayer(Properties.Resources.journeySound);
+        //new SoundPlayer journeySound = new SoundPlayer(Properties.Resources.Menu_Screen_Sound);
+        //new SoundPlayer villageSound = new SoundPlayer(Properties.Resources.medieval_village_atmosphere);
+        new SoundPlayer page1 = new SoundPlayer(Properties.Resources.Page1);
+        
         public Form1()
         {
             InitializeComponent();
+            //introSound.Play();
+            outputLabel.Visible = false;
+            mainPictureBox.Visible = false;
+            option1Button.Visible = false;
+            option2Button.Visible = false;
+            option1Label.Visible = false;
+            option2Label.Visible = false;   
         }
 
         private void playButton_Click(object sender, EventArgs e)
         {
-            int page = 1;
+
+            mainPictureBox.Visible = true;
+            outputLabel.Visible = true;
+            option1Button.Visible = true;
+            option1Label.Visible = true;
+            option2Button.Visible = true;
+            option2Label.Visible = true;
+            //option3Button.Visible = false;
+            //option3Label.Visible = false;
+
+            titleImage.Visible = false;
+            titleLabel.Visible = false;
+            playButton.Visible = false;
+            //introSound.Stop();
+            displayPage(); 
+
         }
         private void displayPage()
         {
             switch (page)
             {
                 case 1:
-                    outputLabel.Text = "You are lost in a forest";
-                    option1Label.Text = "Go North";
-                    option2Label.Text = "Go South";
+                    outputLabel.Text = "The king has picked you to go on a journey to find a relic. Do you want to meet the village elder or meet the blacksmith?";
+                    option1Label.Text = "Meet elder";
+                    option2Label.Text = "Meet blacksmith";
+
+
+                    // journeySound.Play();
+
+
                     break;
+                case 2:
+                    outputLabel.Text = "You recieve a map for the journey. Do you want to leave for the journey or visit the blacksmith?";
+                    option1Label.Text = "Go on journey";
+                    option2Label.Text = "Visit blacksmith";
+                   // villageSound.Play();
+                    break;
+                case 3:
+                    outputLabel.Text = "You recieved a 1 time use potion to kill the monster defending the relic, and you also got suited up to fight and are looking scary. Do you ask the village elder for relic locations or leave for the journey?";
+                    option1Label.Text = "ask for location";
+                    option2Label.Text = "leave for journey";
+                    break;
+                case 4:
+                    outputLabel.Text = "You encountered a monster. Do you want to fight the monster or run away?";
+                    option1Label.Text = "Fight";
+                    option2Label.Text = "Run";
+                    break;
+                case 5:
+                    outputLabel.Text = "You recieved a 1 time use potion to kill the monster defending the relic, you suited up in a very powerful armor, and have tools to fight with. On your journey you hear rustling in the bushes. Do you Investigate the noise or retreat?";
+                    option1Label.Text = "Investigate";
+                    option2Label.Text = "Retreat";
+                    break;
+                case 6:
+                    outputLabel.Text = "You tried acting brave but you didn't have any tools to defeat the monster and you lost. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "You ran away and were unable to do the task the king hires someone else for the job. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 8:
+                    outputLabel.Text = "You died while attempting to run away. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 9:
+                    outputLabel.Text = "It was a harmless animal. It's getting dark. Do you make shelter or keep going to find relic before night fall?";
+                    option1Label.Text = "Make shelter";
+                    option2Label.Text = "Find relic";
+                    break;
+                case 10:
+                    outputLabel.Text = "You ran away and were unable to do the task the king hires someone else for the job. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "You successfully made a temporary shelter for the night. You are hungry when you wake up.Do you go hunting or keep going to find the relic";
+                    option1Label.Text = "Go hunting";
+                    option2Label.Text = "Find relic";
+                    break;
+                case 12:
+                    outputLabel.Text = "It got too dark and couldn't see your path you fell in a pit and died. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "You stumble upon a deer. Eat it raw so you can go back fast or cook it and eat it?";
+                    option1Label.Text = "Eat it raw";
+                    option2Label.Text = "Cook it";
+                    break;
+                case 14:
+                    outputLabel.Text = "You found the cave with the relic. Do you enter the cave or make a torch";
+                    option1Label.Text = "Enter the cave";
+                    option2Label.Text = "Make a torch";
+                    break;
+                case 15:
+                    outputLabel.Text = "You suffered food poisioning and died. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 16:
+                    outputLabel.Text = "While you were cooking a moster attacked you from the back and you died. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 17:
+                    outputLabel.Text = "You entered the cave and encounter the monster lurking behind you. Do you use the potion to kill the monster or use sword becuase this might not be the strongest monster there or do you run past this monster?";
+                    option1Label.Text = "Use potion";
+                    option2Label.Text = "Use sword";
+                    break;
+                case 18:
+                    outputLabel.Text = "You went to find resources for a torch. While trying to wood the tree fell on you and you died. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 19:
+                    outputLabel.Text = "The monster dies and you see a glowing light coming from the relic. You retrive it and give it to the king. Do you tell the king to pay you or request him to hire you for most jobs like this?";
+                    option1Label.Text = "Ask for money";
+                    option2Label.Text = "Ask for job";
+                    break;
+                case 20:
+                    outputLabel.Text = "There was 20 foot fall ahead and you died. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 21:
+                    outputLabel.Text = "You didn't have enough energy to strike the monster and it ended up killing you. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 22:
+                    outputLabel.Text = "The king became evil after possing the power of the relic and kills you. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 23:
+                    outputLabel.Text = "The king hire you and rewards you for this task. Congrats!!! You did it. Do you want to play again and try a different journey?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 24:
+                    outputLabel.Text = "They got scared from you and ran away you failed the mission. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 25:
+                    outputLabel.Text = "You set out for the journey and spent years searching but you gave up and the king killed you for returning empty handed. Play again?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 26:
+                    outputLabel.Text = "Thank you for playing.";
+                    option1Label.Text = "";
+                    option2Label.Text = "";
+                    Thread.Sleep(2000);
+                    Refresh();
+                    Application.Exit();
+                    break;
+
+
+
             }
         }
-        private void option2Button_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             if (page == 1)
             {
-                page = 3;    
+                page = 2;
+                //journeySound.Stop();
             }
-            
+            else if (page == 2)
+            {
+                page = 4;
+                //villageSound.Stop();
+            }
+            else if (page == 3)
+            {
+                page = 24;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 5)
+            {
+                page = 9;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+            else if (page == 7)
+            {
+                page = 1;
+            }
+            else if (page == 8)
+            {
+                page = 1;
+            }
+            else if (page == 9)
+            {
+                page = 11;
+            }
+            else if (page == 10)
+            {
+                page = 1;
+            }
+            else if (page == 11)
+            {
+                page = 13;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+            else if (page == 13)
+            {
+                page = 15;
+            }
+            else if (page == 14)
+            {
+                page = 17;
+            }
+            else if (page == 15)
+            {
+                page = 1;
+            }
+            else if (page == 16)
+            {
+                page = 1;
+            }
+            else if (page == 17)
+            {
+                page = 19;
+            }
+            else if (page == 18)
+            {
+                page = 1;
+            }
+            else if (page == 19)
+            {
+                page = 22;
+            }
+            else if (page == 20)
+            {
+                page = 1;
+            }
+            else if (page == 21)
+            {
+                page = 1;
+            }
+            else if (page == 22)
+            {
+                page = 1;
+            }
+            else if (page == 23)
+            {
+                page = 1;
+            }
+            else if (page == 24)
+            {
+                page = 1;
+            }
+            else if (page == 25)
+            {
+                page = 1;
+            }
+
+            displayPage();
+
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1) 
+            {
+                page = 3;
+                //journeySound.Stop();
+            }
+
             else if (page == 2)
             {
                 page = 5;
+               // villageSound.Stop();
             }
             else if (page == 3)
             {
@@ -52,7 +330,7 @@ namespace AdventureGame
             // Have to code the process here
             else if (page == 4)
             {
-                page = 1;
+                page = 26;
             }
             else if (page == 5)
             {
@@ -60,84 +338,87 @@ namespace AdventureGame
             }
             else if (page == 6)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 7)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 8)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 9)
             {
-                page = 5;
+                page = 12;
             }
             else if (page == 10)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 11)
             {
-                page = 5;
+                page = 14;
             }
             else if (page == 12)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 13)
             {
-                page = 5;
+                page = 16;
             }
             else if (page == 14)
             {
-                page = 5;
+                page = 18;
             }
             else if (page == 15)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 16)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 17)
             {
-                page = 5;
+                page = 21;
             }
             else if (page == 18)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 19)
             {
-                page = 5;
+                page = 23;
             }
             else if (page == 20)
             {
-                page = 5;
+                page = 26;
             }
             else if (page == 21)
             {
-                page = 5;
+                page = 26;
             }
-            
-            
-            
-            
-
-            DisplayPage();
+            else if (page == 22)
+            {
+                page = 26;
+            }
+            else if (page == 23)
+            {
+                page = 26;
+            }
+            else if (page == 24)
+            {
+                page = 26;
+            }
+            else if (page == 25)
+            {
+                page = 26;
+            }
+            displayPage();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void option1Label_Click(object sender, EventArgs e)
-        {
-
-        }
     }
-    }
+}
